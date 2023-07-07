9b569c8de1c2267579958b60d591a13bb6be4643
bdunko <bdunko@gmail.com>
2023-07-07 19:52:53 -0400

    Snake grows, segments follow

--- snake.gd
+++ snake.gd
@@ -6,8 +6,6 @@ enum Direction {
 	UP, DOWN, LEFT, RIGHT
 }
 
-var _direction := Direction.UP
-
 var _ROTATION_DEGREES_BY_DIR = {
 	Direction.UP : 0,
 	Direction.DOWN : 180,
@@ -22,7 +20,15 @@ var _MOVEMENT_BY_DIR = {
 	Direction.RIGHT : Vector2(1, 0)
 }
 
+var _direction := Direction.UP
+var _previous_head_position := Vector2(0, 0)
+var _previous_head_rotation := 0
+
 func _move():
+	# store previous before moving
+	_previous_head_position = position
+	_previous_head_rotation = $HeadSprite.rotation_degrees
+	
 	# get my grid position
 	var grid_pos = Global.to_grid_position(position)
 	
@@ -71,6 +77,25 @@ func _move():
 	position = Global.to_global_position(grid_pos)
 	position.y -= 1 # move up a bit more for the tongue
 	
+	
+	# move each segment to position/rotation of segment in front of it, from back to front, and update z-index
+	var segments = $Segments.get_children()
+	segments.reverse()
+	for i in range(0, segments.size()):
+		var segment = segments[i]
+		
+		# special case: if last segment, move to head's previous
+		if i == segments.size() - 1:
+			segment.position = _previous_head_position
+			segment.rotation_degrees = _previous_head_rotation
+		else: # otherwise move to next segment
+			var next_segment = segments[i+1]
+			segment.position = next_segment.position
+			segment.rotation_degrees = next_segment.rotation_degrees
+		
+		
+		print(i)
+	
 	_update_sprite_rotation()
 
 func _update_sprite_rotation():
@@ -78,3 +103,8 @@ func _update_sprite_rotation():
 
 func _on_pellet_hitbox_area_entered(area: Area2D):
 	area.get_parent().queue_free()
+	# grow snake, add segment at previous location
+	var new_segment = load("res://snake_segment.tscn").instantiate()
+	$Segments.add_child(new_segment)
+	new_segment.rotation_degrees = _previous_head_rotation
+	new_segment.position = _previous_head_position
