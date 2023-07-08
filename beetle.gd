39916a395354174756646240a83d6b5ea739b2e2
bdunko <bdunko@gmail.com>
2023-07-08 14:05:51 -0400

    beetle rotation

--- beetle.gd
+++ beetle.gd
@@ -4,8 +4,21 @@ const BULLET = preload("res://bullet.tscn")
 const BULLET_SPEED = 50
 
 func _process(_delta):
-	pass
-	# rotate in 90d increments
+	#rotate to face player
+	var pellet_pos = get_parent().get_parent().find_child("Pellet").position
+	var x_diff = position.x - pellet_pos.x
+	var y_diff = position.y - pellet_pos.y
+
+	if abs(x_diff) > abs(y_diff): # face left or right
+		if x_diff > 0: 
+			rotation_degrees = -90
+		else:
+			rotation_degrees = 90
+	else: # face up or down
+		if y_diff > 0:
+			rotation_degrees = 0
+		else:
+			rotation_degrees = 180
 
 func _on_shot_timer_timeout():
 	var bullet = BULLET.instantiate()
